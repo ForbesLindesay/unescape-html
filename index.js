4ef9bfc625f70b57d3d4906502393390115075e5
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2015-09-11 20:20:23 +0300

    add commonjs and amd support

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-'use strict'
+'use strict';
 
-/**
- * un-escape special characters in the given string of html.
- *
- * @param  {String} html
- * @return {String}
- */
-
-module.exports = function (html) {
-  return String(html)
-    .replace(/&quot;/g, '"')
-    .replace(/&#39;/g, '\'')
-    .replace(/&lt;/g, '<')
-    .replace(/&gt;/g, '>')
-    .replace(/&amp;/g, '&')
-}
+(function (root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        // CommonJS
+        module.exports = factory(root);
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['unescape-html'], function () {
+            return factory(root);
+        });
+    } else {
+        // Global Variables
+        root.unescapeHtml = factory(root);
+    }
+}(this, function () {
+    /**
+     * un-escape special characters in the given string of html.
+     *
+     * @param  {String} html
+     * @return {String}
+     */
+    return function unescapeHtml(html) {
+        return String(html)
+            .replace(/&quot;/g, '"')
+            .replace(/&#39;/g, '\'')
+            .replace(/&lt;/g, '<')
+            .replace(/&gt;/g, '>')
+            .replace(/&amp;/g, '&');
+    }
+}));
